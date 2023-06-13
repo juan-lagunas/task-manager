e834b151be45c2c2bcec1780ff44bb5a345aa83d
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-13 19:04:19 -0400

    Setup for deployment

--- tasks/settings.py
+++ tasks/settings.py
@@ -143,7 +143,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
