1d172c7e0bede88a2c40b558f7b60be74b14fc58
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-14 22:48:19 -0400

    Setup for deployment

--- tasks/settings.py
+++ tasks/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-!&7@=lt0ot9-z&h0+&52kh(0gqmlg_k1ltka8-ls@&b(1b+wfq'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!&7@=lt0ot9-z&h0+&52kh(0gqmlg_k1ltka8-ls@&b(1b+wfq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
